198210cf4ce386412d467cd256ccfa3da366985b
SmileRR <qing-ran@163.com>
2021-02-16 20:27:46 +0800

    t

---
+++ eureka-client/src/test/java/io/radon/springcloud/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.radon.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
